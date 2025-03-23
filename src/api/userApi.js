db24594efd5332c84efdb4793819377d842ac2fe
federicomosca <federicomosca01@outlook.it>
2025-03-23 20:42:53 +0100

    chiamata al backend

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/users';
+const API_URL = import.meta.env.VITE_API_URL + '/users';
 
 export const getProfile = () => {
-    const token = localStorage.getItem('token');
-    return axios
-      .get('http://localhost:3000/api/users/profile', {
-        headers: { Authorization: `Bearer ${token}` }
-      })
-      .then(res => res.data);
-  };
-  
+  const token = localStorage.getItem('token');
+  return axios
+    .get(`${API_URL}/profile`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(res => res.data);
+};
 
 export const register = (user) => axios.post(`${API_URL}/register`, user).then(res => res.data);
 export const login = (user) => axios.post(`${API_URL}/login`, user).then(res => res.data);
